68ecfa84c3f470deeba2eb044917b0ed3d8c8654
Akbotoev <akbotoevbegzat@gmail.com>
2024-05-07 16:23:58 +0600

    init

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import './App.css'
+import './src/image'
+ export const App = () => {
+  return (
+    <div>
+      <header className="header" id="header">
+        <div className="container">
+          <a href="" className="header__logo">Your Logo</a>
+          <nav className="header__menu">
+            <a href="#" className="header__link">HOME</a>
+            <a href="#" className="header__link">SHOP</a>
+            <a href="#" className="header__link">LOOKBOOK</a>
+            <a href="#" className="header__link">FEATURES</a>
+            <a href="#" className="header__link">PAGES</a>
+            <a href="#" className="header__link">BLOG</a>
+            </nav>
+            <a href="#" className="iconbox"></a>
+            <a href="#" className="iconbox"></a>
+            <a href="#" className="iconbox"></a>
+        </div>
+      </header>
+      <section className='hero'>
+        <div className='cont'>
+         <img src='/'
+        </div>
+
+      </section>
+    </div>
+  );
+}
+
+
